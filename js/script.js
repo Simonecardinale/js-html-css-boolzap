992850ba8b2d1568e3f82db0a0e196060a3dd9ea
Simone Cardinale <simonecardinale8@gmail.com>
2021-02-02 10:58:37 +0100

    avatar active WIP

--- js/script.js
+++ js/script.js
@@ -4,21 +4,33 @@ var app = new Vue ({
         avatarArray: [
             {
                 nome: 'Michele',
-                immagine: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png'
+                immagine: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png',
+                status: false,
             },
             {
                 nome: 'Federica',
-                immagine: "https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807__340.png"
+                immagine: "https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807__340.png",
+                status: false,
             },
             {
                 nome: 'Nino',
-                immagine: "https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331__340.png"
+                immagine: "https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331__340.png",
+                status: false,
             },
             {
                 nome: 'Peppe',
-                immagine: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png"
+                immagine: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png",
+                status: false,
             },
             
         ]
+    },
+
+    methods: {
+        statusChange(index) {
+            this.avatarArray[index].status = true;
+            console.log(this.avatarArray[0].status);
+            console.log(this.avatarArray[1].status);
+        }
     }
 });
