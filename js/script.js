25e9b1e6a5a43224b2a7375ff55b998c46094b13
Simone Cardinale <simonecardinale8@gmail.com>
2021-02-02 10:30:45 +0100

    left avatar list completed

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+        avatarArray: [
+            {
+                nome: 'Michele',
+                immagine: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png'
+            },
+            {
+                nome: 'Federica',
+                immagine: "https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807__340.png"
+            },
+            {
+                nome: 'Nino',
+                immagine: "https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331__340.png"
+            },
+            {
+                nome: 'Peppe',
+                immagine: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png"
+            },
+            
+        ]
+    }
+});
