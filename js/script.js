8c4c3f9077dd8f3aed0c23f21587940b7e0ec89e
Simone Cardinale <simonecardinale8@gmail.com>
2021-02-03 11:59:24 +0100

    milestone 2 complete

--- js/script.js
+++ js/script.js
@@ -71,8 +71,8 @@ var app = new Vue ({
                 immagine: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png",
                 status: false,
                 message:[
-                    {
-                        message_sent: "Stasera serata gaming",
+                    {   
+                        message_sent: "",
                         message_received: "AoE?",
                         sent: "sent",
                         received: "received"
