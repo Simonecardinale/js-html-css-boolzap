8342ae0db69f76cda951ab573b8e4b9c2a8c4737
Simone Cardinale <simonecardinale8@gmail.com>
2021-02-04 19:51:25 +0100

    milestone 2 complete with last access

--- js/script.js
+++ js/script.js
@@ -4,97 +4,115 @@ var app = new Vue ({
         user:[
             {
                 nome: "Simone",
-                immagine: "https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png"
+                immagine: "https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png",
+                date: ""
             }
         ],
         avatarArray: [
             {
                 nome: 'Michele',
                 immagine: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png',
-                status: false,
+                
                 message:[
                     {
                         text: "Ciao come stai?",
                         status: "sent",
+                        date: '20/03/2020 16:30:00',
                     },
                     {
                         text: "Tutto bene tu?",
                         status: "received",
+                        date: '22/03/2020 18:30:00',
                     },
                     {
                         text: "Tutto ok!",
                         status: "sent",
-                    }
+                        date: '23/03/2020 06:30:00',
+                    },
+                    
                 ],
                 
             },
             {
                 nome: 'Federica',
                 immagine: "https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807__340.png",
-                status: false,
+                
                 message:[
                     {
                         text: "Domani a pesca?",
                         status: "sent",
+                        date: '27/03/2020 19:30:00',
                     },
                     {
                         text: "No mi secca",
                         status: "received",
+                        date: '28/03/2020 10:30:00',
                     },
                     {
                         text: "Va bene",
                         status: "sent",
+                        date: '28/03/2020 11:30:00',
                     }
                 ],
             },
             {
                 nome: 'Nino',
                 immagine: "https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331__340.png",
-                status: false,
+                
                 message:[
                     {
                         text: "Aperitivo",
                         status: "sent",
+                        date: '18/03/2020 16:30:00',
                     },
                     {
                         text: "Si dai",
                         status: "received",
+                        date: '19/03/2020 18:30:00',
                     },
                     {
                         text: "Andiamo!",
                         status: "sent",
-                    },
+                        date: '21/03/2020 16:30:00',
+                    }
                 ],
             },
             {
                 nome: 'Peppe',
                 immagine: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png",
-                status: false,
+                
                 message:[
                     {
                         text: "AoE?",
                         status: "sent",
+                        date: '20/03/2020 10:30:00',
                     },
                     {
                         text: "Ti distruggo",
                         status: "received",
+                        date: '20/03/2020 19:30:00',
                     },
                     {
                         text: "Si ciao!",
                         status: "sent",
-                    }
+                        date: '20/03/2020 20:30:00',
+                    },
+                    {
+                        text: "Ciao",
+                        status: "received",
+                        date: '20/03/2020 22:30:00',
+                    },
                 ]
             },
         ],
         chat: "",
         utenteSelezionato: 0,
-        risposta: "",
+        indexData: "",
     },
 
     created() {
         this.utenteSelezionato = this.avatarArray[0];
-        console.log(this.utenteSelezionato);
-        
+        this.indexData = this.utenteSelezionato.message[0].date;
     },
 
 
@@ -107,7 +125,13 @@ var app = new Vue ({
 
         indexContatto(index) {
             this.utenteSelezionato = this.avatarArray[index];
-            console.log(this.utenteSelezionato);
+
+            for(var k in this.utenteSelezionato.message) {
+                if(this.utenteSelezionato.message[k].status == "received") {
+                    this.indexData = this.utenteSelezionato.message[k].date;
+                }
+            }
+            console.log(this.indexData);
         },
         
         sendChat() {
@@ -117,8 +141,7 @@ var app = new Vue ({
             },
             sendAnswer(){
                 this.utenteSelezionato.message.push({text:"ok", status: "received"})
-                console.log(this.utenteSelezionato);
-            }
+            },
         },
 
     
